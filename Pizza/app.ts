d6c4d6f031c1243ddf617e5f0bdf2027f3e38f18
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 06:00:13 +0500

    Add files via upload

---
+++ Pizza/app.ts
@@ -0,0 +1,6 @@
+let pizza: string[] = ["pepperoni", "fajita", "margherita"];
+for (let i = 0; i < pizza.length; i++) {
+    console.log(`I like ${pizza[i]} pizza`);
+}
+console.log(" This pizza is topped with fajita-style grilled chicken, onions, bell peppers, and a blend of spices.  This may not come as a surprise, but pepperoni is by far the most popular pizza topping in the Pakistan.It is relatively cheap and easy to make, it is versatile, convenient, and can be customized to suit different tastes.");
+console.log("I really love pizza");
