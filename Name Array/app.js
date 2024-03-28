466a1f438923d5e797f48def0021136889b14d89
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:40:22 +0500

    Add files via upload

---
+++ Name Array/app.js
@@ -0,0 +1,7 @@
+// Define an array of names
+var names = ["Ali", "Bobby", "Chahal", "Dawood", "Saim"];
+// Print each person's name using a for...of loop
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log(name_1);
+}
---
+++ Name Array/app.ts
@@ -0,0 +1,7 @@
+// Define an array of names
+let names: string[] = ["Ali", "Bobby", "Chahal", "Dawood", "Saim"];
+
+// Print each person's name using a for...of loop
+for (let name of names) {
+    console.log(name);
+}
