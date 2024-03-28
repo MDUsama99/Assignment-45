5526bb69311c5fb73221cb2d8bd577ef99d92fb8
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:54:30 +0500

    Add files via upload

---
+++ Alien color 2/app.js
@@ -0,0 +1,16 @@
+//If statement works
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player earned 5 Point! for shooting the alien");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
+//Else statement works
+var alien_color1 = 'green';
+if (alien_color1 === 'yellow') {
+    console.log("Player earned 5 Point! for shooting the alien");
+}
+else {
+    console.log("Player just earned 10 points. ");
+}
---
+++ Alien color 2/app.ts
@@ -0,0 +1,15 @@
+//If statement works
+let alien_color: string = 'green';
+if (alien_color === 'green') {
+    console.log("Player earned 5 Point! for shooting the alien");
+}   else {
+    console.log("Player just earned 10 points.");
+}
+
+//Else statement works
+let alien_color1: string = 'green';
+if (alien_color1 === 'yellow') {
+    console.log("Player earned 5 Point! for shooting the alien");
+}   else {
+    console.log("Player just earned 10 points. ");
+}
