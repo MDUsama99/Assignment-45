d008f709d3481d5c000a1667c340d63e9d51ef06
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:49:36 +0500

    Add files via upload

---
+++ conditional test/app.ts
@@ -0,0 +1,31 @@
+let x: number = 25
+// Test 1: Is x equal to 25? I predict True.
+console.log("Is x is equal to 25? I predict true.");
+console.log( x == 25 );
+// Test 2: Is x less than to 55? I predict True.
+console.log("Is x is less than to 55? I predict true.");
+console.log( x < 55 );
+// Test 3: Is x greater than 20? I predict True.
+console.log("Is x is greater than to 20? I predict true.");
+console.log( x > 20 );
+// Test 4: Is x less than or equal to 25? I predict True.
+console.log("Is x less than or equal to 25? I predict true.");
+console.log( x <= 25 );
+// Test 5: Is x strictly equal to '25'? I predict true.
+console.log("Is x is stricly equal to 25? I predict true.");
+console.log( x === 25 );
+// Test 6: Is x not equal to 25? I predict False.
+console.log("Is x is not equal to 25? I predict false.");
+console.log( x != 25 );
+// Test 7: Is x greater than 33? I predict False.
+console.log("Is x is greater than to 33? I predict false.");
+console.log( x > 33 );
+// Test 8: Is x greater than or equal to 77? I predict False.
+console.log("Is x is greater than or equal to 77? I predict false.");
+console.log( x >= 77 );
+// Test 9: Is x strictly equal to '5'? I predict False.
+console.log("Is x is equal to 7 and less than 10? I predict false.");
+console.log(x === 7 && x < 10);
+// Test 10: Is x not equal to 5? I predict false.
+console.log("Is x is greater than 9 or equal to 7? I predict false.");
+console.log( x > 9 || x === 9 );
