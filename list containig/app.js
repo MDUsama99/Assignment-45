d8d2e4fddb71d0b87bacdc65c381082c9dc6276f
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:47:38 +0500

    Add files via upload

---
+++ list containig/app.js
@@ -0,0 +1,5 @@
+var countries = ["Pakistan", "India", "Bangladash", "Srilanka", "Afghanistan"];
+console.log("\nCountries Name:");
+for (var i = 0; i < countries.length; i++) {
+    console.log(countries[i]);
+}
---
+++ list containig/app.ts
@@ -0,0 +1,5 @@
+let countries: string[] = ["Pakistan", "India", "Bangladash", "Srilanka", "Afghanistan"];
+console.log("\nCountries Name:");
+for (let i=0; i < countries.length; i++) {
+    console.log(countries[i]);
+}
