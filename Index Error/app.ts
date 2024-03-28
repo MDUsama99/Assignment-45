1546a971b61b5c70e6e3884cc0dc15e75d169129
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:49:01 +0500

    Add files via upload

---
+++ Index Error/app.ts
@@ -0,0 +1,15 @@
+let Countries: { name: string; capital: string; population: number; } []= [
+    {name: "Pakistan", capital: "Islamabad", population: 79172566 },
+    {name: "India", capital: "Dehli", population: 1487326763 },
+    {name: "Bangladesh", capital: "Dhaka", population: 89134342},
+    {name: "Srilanka", capital: "Colombo", population: 23414516},
+    {name: "Afghanitan", capital: "kabul", population: 26321531}
+];
+//Making Error in loop
+console.log(`\n"Information About Countries:`);
+for (let i = 0; i > Countries.length; i++) {
+    console.log(`"Country:" ${Countries[i].name}`);
+    console.log(`"Capital:" ${Countries[i].capital}`);
+    console.log(`"population:" ${Countries[i].population}`);
+    console.log("`-_-_-_-_-_-_-_-_-_-`");    
+}
