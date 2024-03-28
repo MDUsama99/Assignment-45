598e2da18bcbee096829845dad87d2f3f9a41d3b
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:53:23 +0500

    Add files via upload

---
+++ conditional test/allien color/app.js
@@ -0,0 +1,22 @@
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+else {
+    console.log("No points earned");
+}
+//other version
+var Alien_color = 'yellow';
+if (Alien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+else {
+    console.log("No points earned");
+}
+// No Output
+var Allien_color = 'yellow';
+if (Allien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+else {
+}
---
+++ conditional test/allien color/app.ts
@@ -0,0 +1,24 @@
+let alien_color: string = 'green';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+    else {
+        console.log("No points earned");
+    }
+//other version
+let Alien_color: string= 'yellow';
+if (Alien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+    else {
+        console.log("No points earned");
+    }
+
+    // No Output
+    let Allien_color: string= 'yellow';
+if (Allien_color === 'green') {
+    console.log("Player just earned 5 points!");
+}
+    else {
+        
+    }
