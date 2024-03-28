5c373d63b0576225d3cd792af3094b3d113e089b
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:55:56 +0500

    Add files via upload

---
+++ Stages of Life/app.js
@@ -0,0 +1,161 @@
+//Person is a Baby
+var age = 1;
+if (age < 2) {
+    console.log("person is a baby:");
+}
+else if (age < 4) {
+    console.log("person is a toddler:");
+}
+else if (age < 13) {
+    console.log("person is a kid:");
+}
+else if (age < 20) {
+    console.log("person is a teenager:");
+}
+else if (age < 65) {
+    console.log("person is a adult:");
+}
+else if (age > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
+//person is a toddler
+var age1 = 3;
+if (age1 < 2) {
+    console.log("person is a baby:");
+}
+else if (age1 < 4) {
+    console.log("person is a toddler:");
+}
+else if (age1 < 13) {
+    console.log("person is a kid:");
+}
+else if (age1 < 20) {
+    console.log("person is a teenager:");
+}
+else if (age1 < 65) {
+    console.log("person is a adult:");
+}
+else if (age1 > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
+//Person is a Kid
+var age2 = 12;
+if (age2 < 2) {
+    console.log("person is a baby:");
+}
+else if (age2 < 4) {
+    console.log("person is a toddler:");
+}
+else if (age2 < 13) {
+    console.log("person is a kid:");
+}
+else if (age2 < 20) {
+    console.log("person is a teenager:");
+}
+else if (age2 < 65) {
+    console.log("person is a adult:");
+}
+else if (age2 > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
+//Person is a Teenager
+var age3 = 18;
+if (age3 < 2) {
+    console.log("person is a baby:");
+}
+else if (age3 < 4) {
+    console.log("person is a toddler:");
+}
+else if (age3 < 13) {
+    console.log("person is a kid:");
+}
+else if (age3 < 20) {
+    console.log("person is a teenager:");
+}
+else if (age3 < 65) {
+    console.log("person is a adult:");
+}
+else if (age3 > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
+//Person is a Adult
+var age4 = 25;
+if (age < 2) {
+    console.log("person is a baby:");
+}
+else if (age4 < 4) {
+    console.log("person is a toddler:");
+}
+else if (age4 < 13) {
+    console.log("person is a kid:");
+}
+else if (age4 < 20) {
+    console.log("person is a teenager:");
+}
+else if (age4 < 65) {
+    console.log("person is a adult:");
+}
+else if (age4 > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
+//Person is an Elder
+var age5 = 68;
+if (age5 < 2) {
+    console.log("person is a baby:");
+}
+else if (age5 < 4) {
+    console.log("person is a toddler:");
+}
+else if (age5 < 13) {
+    console.log("person is a kid:");
+}
+else if (age5 < 20) {
+    console.log("person is a teenager:");
+}
+else if (age5 < 65) {
+    console.log("person is a adult:");
+}
+else if (age5 > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
+//Invalid Number 
+var age0 = -2;
+if (age0 < 2) {
+    console.log("person is a baby:");
+}
+else if (age0 < 4) {
+    console.log("person is a toddler:");
+}
+else if (age0 < 13) {
+    console.log("person is a kid:");
+}
+else if (age0 < 20) {
+    console.log("person is a teenager:");
+}
+else if (age0 < 65) {
+    console.log("person is a adult:");
+}
+else if (age0 > 65) {
+    console.log("person is an elder:");
+}
+else {
+    console.log("invalid age:");
+}
---
+++ Stages of Life/app.ts
@@ -0,0 +1,119 @@
+//Person is a Baby
+let age : number = 1;
+if (age < 2) {
+    console.log("person is a baby:");
+} else if(age < 4 ) {
+    console.log("person is a toddler:");
+} else if(age < 13) {
+    console.log("person is a kid:");
+} else if(age < 20) {
+    console.log("person is a teenager:");
+} else if(age < 65) {
+    console.log("person is a adult:");
+} else if(age > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
+//person is a toddler
+let age1 : number = 3;
+if (age1 < 2) {
+    console.log("person is a baby:");
+} else if(age1 < 4 ) {
+    console.log("person is a toddler:");
+} else if(age1 < 13) {
+    console.log("person is a kid:");
+} else if(age1 < 20) {
+    console.log("person is a teenager:");
+} else if(age1 < 65) {
+    console.log("person is a adult:");
+} else if(age1 > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
+//Person is a Kid
+let age2 : number = 12;
+if (age2 < 2) {
+    console.log("person is a baby:");
+} else if(age2 < 4 ) {
+    console.log("person is a toddler:");
+} else if(age2 < 13) {
+    console.log("person is a kid:");
+} else if(age2 < 20) {
+    console.log("person is a teenager:");
+} else if(age2 < 65) {
+    console.log("person is a adult:");
+} else if(age2 > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
+//Person is a Teenager
+let age3 : number = 18;
+if (age3 < 2) {
+    console.log("person is a baby:");
+} else if(age3 < 4 ) {
+    console.log("person is a toddler:");
+} else if(age3 < 13) {
+    console.log("person is a kid:");
+} else if(age3 < 20) {
+    console.log("person is a teenager:");
+} else if(age3 < 65) {
+    console.log("person is a adult:");
+} else if(age3 > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
+//Person is a Adult
+let age4 : number = 25;
+if (age < 2) {
+    console.log("person is a baby:");
+} else if(age4 < 4 ) {
+    console.log("person is a toddler:");
+} else if(age4 < 13) {
+    console.log("person is a kid:");
+} else if(age4 < 20) {
+    console.log("person is a teenager:");
+} else if(age4 < 65) {
+    console.log("person is a adult:");
+} else if(age4 > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
+//Person is an Elder
+let age5 : number = 68;
+if (age5 < 2) {
+    console.log("person is a baby:");
+} else if(age5 < 4 ) {
+    console.log("person is a toddler:");
+} else if(age5 < 13) {
+    console.log("person is a kid:");
+} else if(age5 < 20) {
+    console.log("person is a teenager:");
+} else if(age5 < 65) {
+    console.log("person is a adult:");
+} else if(age5 > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
+//Invalid Number 
+let age0 : number = -2;
+if (age0 < 2) {
+    console.log("person is a baby:");
+} else if(age0 < 4 ) {
+    console.log("person is a toddler:");
+} else if(age0 < 13) {
+    console.log("person is a kid:");
+} else if(age0 < 20) {
+    console.log("person is a teenager:");
+} else if(age0 < 65) {
+    console.log("person is a adult:");
+} else if(age0 > 65) {
+    console.log("person is an elder:");
+} else {
+    console.log("invalid age:");
+}
