a9806d2d691f905e180c9e3b7516c67d0945979c
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:39:35 +0500

    Add files via upload

---
+++ Adding Comment/app.ts
@@ -0,0 +1,5 @@
+//Adding Comment
+//My Name is USAMA
+//Dated: 20/2/2024
+//this program just run simple code "hello world"
+console.log('hello world! Simple Program');
---
+++ Adding Comment/program2.js
@@ -0,0 +1,5 @@
+//Adding Comment
+//My Name is USAMA
+//Dated: 20/2/2024
+//this program will multiply both number 5 & 3
+console.log(5 + 3);
