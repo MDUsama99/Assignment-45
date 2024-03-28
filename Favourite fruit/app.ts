b67d8dc2674fa3b785407e41e2c477fd60e38bd7
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:56:45 +0500

    Add files via upload

---
+++ Favourite fruit/app.ts
@@ -0,0 +1,25 @@
+let favourite_fruits: string[] = ["apple", "banana", "mango"];
+
+if (favourite_fruits.includes("apple")) {
+    console.log("I really like apples!");
+}
+
+if (favourite_fruits.includes("banana")) {
+    console.log("I really like banana!");
+}
+
+if (favourite_fruits.includes("mango")) {
+    console.log("I really like mango!");
+}
+
+if (favourite_fruits.includes("grapes")) {
+    console.log("I really like grapes!");
+} else {
+    console.log("I don't really like grapes!");
+}
+
+if (favourite_fruits.includes("kiwi")) {
+    console.log("I really like kiwi!");
+} else {
+    console.log("I don't really like kiwi!");
+}
---
+++ Favourite fruit/main.ts
@@ -0,0 +1,27 @@
+// Favorite Fruits
+let favorite_fruits: string[] = ["apple", "banana", "orange"];
+
+// Checking for Favorite Fruits using indexOf
+if (favorite_fruits.indexOf("banana") !== -1) {
+    console.log("You really like bananas!");
+}
+
+if (favorite_fruits.indexOf("apple") !== -1) {
+    console.log("You really like apples!");
+}
+
+if (favorite_fruits.indexOf("orange") !== -1) {
+    console.log("You really like oranges!");
+}
+
+if (favorite_fruits.indexOf("grape") !== -1) {
+    console.log("You really like grapes!");
+} else {
+    console.log("You don't like grapes that much.");
+}
+
+if (favorite_fruits.indexOf("kiwi") !== -1) {
+    console.log("You really like kiwis!");
+} else {
+    console.log("You don't like kiwis that much.");
+}
