4a2e31cbc82ef75ef5bdb0e944930d4df474ae58
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:42:49 +0500

    Add files via upload

---
+++ Guest list/app.js
@@ -0,0 +1,5 @@
+var Myguest = ["Ghazanfer", "Arif", "Tabish"];
+for (var _i = 0, Myguest_1 = Myguest; _i < Myguest_1.length; _i++) {
+    var guest = Myguest_1[_i];
+    console.log("Dear! ".concat(guest, " I would like to invite you on dinner, we would be honored to have your presence."));
+}
---
+++ Guest list/app.ts
@@ -0,0 +1,4 @@
+let Myguest: string[] = ["Ghazanfer","Arif","Tabish"];
+for (let guest of Myguest) {
+   console.log(`Dear! ${guest} I would like to invite you on dinner, we would be honored to have your presence.`);
+}
