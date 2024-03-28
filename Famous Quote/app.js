fa008f95a60b7a030902b016477c7f11fd9ae8b2
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:34:11 +0500

    Add files via upload

---
+++ Famous Quote/app.js
@@ -0,0 +1,6 @@
+// let qoute: string= "A person who never made a mistake never tried anything new";
+// let author: string= "Albert Einstein"
+// console.log(`${author} once said, "${qoute}`);
+var qoute = "A person who never made a mistake never tried anything new";
+var famous_person = "Albert Einstein";
+console.log("".concat(famous_person, " once said, \"").concat(qoute));
---
+++ Famous Quote/app.ts
@@ -0,0 +1,8 @@
+// let qoute: string= "A person who never made a mistake never tried anything new";
+// let author: string= "Albert Einstein"
+// console.log(`${author} once said, "${qoute}`);
+
+let qoute: string= "A person who never made a mistake never tried anything new";
+let famous_person: string= "Albert Einstein";
+console.log(`${famous_person} once said, "${qoute}`);
+
