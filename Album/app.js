a941f39b92db02d34d010170c91a0b9c8ae2c9f3
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 06:04:25 +0500

    Add files via upload

---
+++ Album/app.js
@@ -0,0 +1,9 @@
+function album(artist, title) {
+    return "".concat(artist, ", ").concat(title);
+}
+var album0 = album("artist0", "title0");
+var album1 = album("artist1", "title1");
+var album2 = album("artist2", "title2");
+console.log(album0);
+console.log(album1);
+console.log(album2);
---
+++ Album/app.ts
@@ -0,0 +1,11 @@
+function album(artist: string, title: string): string {
+    return `${artist}, ${title}`
+} 
+
+let album0 = album("artist0", "title0");
+let album1 = album("artist1", "title1");
+let album2 = album("artist2", "title2");
+
+console.log(album0);
+console.log(album1);
+console.log(album2);
