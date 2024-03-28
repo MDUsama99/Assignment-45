33ace32df53d2d0f38c5a50fc3ded37e4c3f1183
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:55:17 +0500

    Add files via upload

---
+++ Alien color 2/Alien color 3/app.js
@@ -0,0 +1,56 @@
+//If  Green Alien
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player earned 5 Point!");
+}
+else if (alien_color === 'yellow') {
+    console.log("Player earned 10 points!");
+}
+else if (alien_color === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
+//If Yellow Alien
+var alien_color1 = 'yellow';
+if (alien_color1 === 'green') {
+    console.log("Player earned 5 Point!");
+}
+else if (alien_color1 === 'yellow') {
+    console.log("Player earned 10 points!");
+}
+else if (alien_color1 === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
+//If Red Alien
+var alien_color2 = 'red';
+if (alien_color2 === 'green') {
+    console.log("Player earned 5 Point!");
+}
+else if (alien_color2 === 'yellow') {
+    console.log("Player earned 10 points!");
+}
+else if (alien_color2 === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
+//If No Alien shot?
+var alien_color3 = 'No Alien';
+if (alien_color3 === 'green') {
+    console.log("Player earned 5 Point!");
+}
+else if (alien_color3 === 'yellow') {
+    console.log("Player earned 10 points!");
+}
+else if (alien_color3 === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
---
+++ Alien color 2/Alien color 3/app.ts
@@ -0,0 +1,51 @@
+//If  Green Alien
+let alien_color: string = 'green';
+if (alien_color === 'green') {
+    console.log("Player earned 5 Point!");
+} else if(alien_color === 'yellow') {
+    console.log("Player earned 10 points!");
+} else if(alien_color === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
+
+//If Yellow Alien
+let alien_color1: string = 'yellow';
+if (alien_color1 === 'green') {
+    console.log("Player earned 5 Point!");
+} else if(alien_color1 === 'yellow') {
+    console.log("Player earned 10 points!");
+} else if(alien_color1 === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
+
+//If Red Alien
+let alien_color2: string = 'red';
+if (alien_color2 === 'green') {
+    console.log("Player earned 5 Point!");
+} else if(alien_color2 === 'yellow') {
+    console.log("Player earned 10 points!");
+} else if(alien_color2 === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
+
+//If No Alien shot?
+let alien_color3: string = 'No Alien';
+if (alien_color3 === 'green') {
+    console.log("Player earned 5 Point!");
+} else if(alien_color3 === 'yellow') {
+    console.log("Player earned 10 points!");
+} else if(alien_color3 === 'red') {
+    console.log("Player earned 15 points!");
+}
+else {
+    console.log("Player earned no points.");
+}
