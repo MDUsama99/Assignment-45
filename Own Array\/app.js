a28453ca95a10890d8c5b3cbd8066efbec11f070
Muhammad Usama <99136930+MDUsama99@users.noreply.github.com>
2024-03-28 05:42:10 +0500

    Add files via upload

---
+++ Own Array\/app.js
@@ -0,0 +1,11 @@
+// let MyArray: string[] = ["car", "airplane", "Cruise", "jet", "Motorcycle"];
+// for (let Names of MyArray) {
+//     console.log(`I would like to be on ${Names}`);
+// }
+// Define an array of favorite modes of transportation
+var favoriteTransportation = ["motorcycle", "car", "bicycle", "bus", "train"];
+// Print statements about these modes of transportation
+for (var _i = 0, favoriteTransportation_1 = favoriteTransportation; _i < favoriteTransportation_1.length; _i++) {
+    var transportation = favoriteTransportation_1[_i];
+    console.log("I would like to own a ".concat(transportation, "."));
+}
---
+++ Own Array\/app.ts
@@ -0,0 +1,4 @@
+let MyArray: string[] = ["car", "airplane", "Cruise", "jet", "Motorcycle"];
+for (let Names of MyArray) {
+    console.log(`I would like to own a ${Names}`);
+}
